e8555662f096dd6cb822c4ae61ce6fe9ba110581
Leonardo de Souza <leosouza1392@gmail.com>
2021-11-23 19:10:36 -0300

    Configured methods and adding numbers to the display

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { View, Text, StyleSheet, Dimensions, TouchableHighlight } from 'react-native';
 
 export default props => {
+    const stylesButton = [styles.button];
+    if(props.double) stylesButton.push(styles.buttonDouble);
+    if(props.triple) stylesButton.push(styles.buttonTriple);
+    if(props.operation) stylesButton.push(styles.operationButton);
     return(
-        <TouchableHighlight style={styles.button} onPress={props.onClick}>
-            <Text style={styles.buttonText}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     );
 }
@@ -19,9 +23,17 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888',
+        fontSize: 40,
         
     },
-    buttonText: {
-        fontSize: 40,
-    }
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple:{
+        width: (Dimensions.get('window').width / 4) * 3,
+    },
 });
