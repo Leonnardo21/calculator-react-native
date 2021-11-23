b9106c32f4592d068daed6310629efd85c09b3e3
Leonardo de Souza <leosouza1392@gmail.com>
2021-11-23 19:45:25 -0300

    Method of adding and validating digit on display implemented

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-
 import React from 'react';
 import {  View, Text, StyleSheet } from 'react-native';
 import Button from './src/components/Button/Button';
 import Display from './src/components/Display/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0 , 0],
+  current: 0,
+}
+
 export default class App extends React.Component {
 
-  state = {
-    displayValue: '0'
-  }
+  state = { ...initialState };
 
   addDigit = n => {
-    this.setState({ displayValue: n});
+    if(n === '.' && this.state.displayValue.includes('.')){
+      return;
+    }
+
+    const clearDisplay = this.state.displayValue === '0' ||
+    this.state.clearDisplay;
+    
+    const currentValue = clearDisplay ? '': this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if(n !== '.'){
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({ values });
+    }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: 0})
+    this.setState({ ...initialState });
   }
 
   setOperation = operation => {
